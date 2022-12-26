85c6997103d17e8065a814e202c4334b267e0560
marshalnanda <mknanda678@gmail.com>
2022-12-27 01:48:16 +0530

    Error events, tracing

---
+++ Error Events/WebForm1.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1.Error_Events
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+            ds.ReadXml(Server.MapPath("~/Data/Countries.xml"));
+
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+        }
+
+        protected void Page_Error(object sender, EventArgs e)
+        {
+            // Get the exception details and log it in the database or event viewer
+            Exception ex = Server.GetLastError();
+            // Clear the exception
+            Server.ClearError();
+            // Redirect user to Error page
+            Response.Redirect("Error.aspx");
+        }
+    }
+}
